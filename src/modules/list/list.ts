8fffa7e2f97de57eee2fff09c6b4364d3c95b4f6
michel silva <michelalmeidasilva@outlook.pt>
2025-02-02 22:33:59 -0300

    feat: (linked-list): added a method to add to front and add to end

--- src/modules/list/list.ts
+++ src/modules/list/list.ts
@@ -1,21 +1,75 @@
+/**
+ *
+ */
 export class NodeL<T> {
-  constructor(public data: T) {}
+  public next: NodeL<T> | null = null;
+  public previous: NodeL<T> | null = null;
+
+  constructor(public value: T) {}
 }
 
+/**
+ *
+ * Linked List:
+ *
+ * Best:
+ *   Read: O(1)
+ *   Insert: O(1) ( Head or Tail)
+ *   Delete: O(1)
+ * Worst:
+ *   Read: O(n) - traverse list
+ *   Insert: O(n) - traverse list
+ *   Delete: O(n) - traverse list
+ */
 export class LinkedList<T> {
-  private readonly head: NodeL<T> | null = null;
+  private head: NodeL<T> | null = null;
+  private tail: NodeL<T> | null = null;
 
   constructor(data?: T) {
     if (data) {
       this.head = new NodeL(data);
+      this.tail = new NodeL(data);
     }
+  }
+
+  addToFront(data: T) {
+    const newNode = new NodeL(data);
+    newNode.next = this.head;
+
+    if (this.head) {
+      this.head.previous = newNode;
+    }
+
+    if (!this.head) {
+      this.tail = newNode;
+    }
+
+    this.head = newNode;
+  }
+
+  addToEnd(data: T) {
+    const newNode = new NodeL(data);
+    newNode.previous = this.tail;
+
+    if (this.tail) {
+      this.tail.next = newNode;
+    }
+
+    if (!this.tail) {
+      this.head = newNode;
+    }
+
+    this.tail = newNode;
   }
 
   isEmpty() {
     return this.head === null;
   }
 
-  getList() {
+  getHead() {
     return this.head;
+  }
+  getTail() {
+    return this.tail;
   }
 }
--- src/modules/list/tests/list.test.ts
+++ src/modules/list/tests/list.test.ts
@@ -3,27 +3,66 @@ import { describe, it, expect } from 'vitest';
 import { LinkedList } from '../list';
 
 describe('LinkedList', () => {
-  it('Should instanciate the linked list with no values', () => {
-    const list = new LinkedList();
+  describe('Instance cases', () => {
+    it('Should instanciate the linked list with no values', () => {
+      const list = new LinkedList();
 
-    expect(list.getList()).toBe(null);
+      expect(list.getHead()).toBe(null);
+    });
+
+    it('Should return isEmpty true when instanced a list with no values', () => {
+      const list = new LinkedList();
+
+      expect(list.isEmpty()).toBe(true);
+    });
+
+    it('Should instanciate the linked list with values', () => {
+      const list = new LinkedList('10');
+
+      expect(list.getHead()?.value).toBe('10');
+    });
+
+    it('Should return isEmpty false when instanced a list with values', () => {
+      const list = new LinkedList('10');
+
+      expect(list.isEmpty()).toBe(false);
+    });
   });
 
-  it('Should return isEmpty true when instanced a list with no values', () => {
-    const list = new LinkedList();
+  describe('Add cases', () => {
+    it('Should add two items with correct prev and next values', () => {
+      const list = new LinkedList();
 
-    expect(list.isEmpty()).toBe(true);
-  });
+      list.addToFront(10);
+      list.addToFront(120);
 
-  it('Should instanciate the linked list with values', () => {
-    const list = new LinkedList('10');
+      expect(list.getHead()?.value).toBe(120);
+      expect(list.getHead()?.next?.value).toBe(10);
+      expect(list.getHead()?.next?.previous?.value).toBe(120);
+    });
 
-    expect(list.getList()?.data).toBe('10');
-  });
+    it('Should add a item at head', () => {
+      const list = new LinkedList();
 
-  it('Should return isEmpty false when instanced a list with values', () => {
-    const list = new LinkedList('10');
+      list.addToFront(10);
 
-    expect(list.isEmpty()).toBe(false);
+      expect(list.getHead()?.value).toBe(10);
+
+      expect(list.getHead()?.next).toBe(null);
+
+      expect(list.isEmpty()).toBe(false);
+    });
+
+    it('Should add a item at Tail', () => {
+      const list = new LinkedList();
+
+      list.addToEnd(10);
+
+      expect(list.getTail()?.value).toBe(10);
+
+      expect(list.getTail()?.previous).toBe(null);
+
+      expect(list.isEmpty()).toBe(false);
+    });
   });
 });
